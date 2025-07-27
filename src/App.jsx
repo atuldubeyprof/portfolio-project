1cfeefc14296203a472aa261f932e895acddc7cd
Atul Dubey <atuldubeyprof@gmail.com>
2025-07-27 11:46:41 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="port">
+        <div className="card">
+          <h1>Atul Dubey</h1>
+          <p>Email: <a href="mailto:atuldubeyprof@gmail.com">atuldubeyprof@gmail.com</a></p>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
